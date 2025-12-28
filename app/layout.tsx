dd2098eea7467cac7865cf3fda7627b547ae743a
Benas Stanulis <benas@pickit.lt>
2025-12-28 22:58:20 +0200

    Changed the initial layout to contain TopNav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopNav from '@/src/components/TopNav';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,18 +18,17 @@ export const metadata: Metadata = {
   description: "Eneba's internship task's frontend application created by Benas Stanulis",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex flex-1 flex-col overflow-hidden px-4 py-6">
+          <div className="flex-none w-full">
+            <TopNav />
+          </div>
+          <div className="flex-1 overflow-y-auto p-6 md:p-12">{children}</div>
+        </div>
       </body>
     </html>
-  );
+  );  
 }
